1070d0dd8fb56485fd67e9b5a3a14397f49f61ab
cqusangharsha <s.chaulagain@cqumail.com>
2023-10-11 15:45:47 +1100

    admin ui transition for doctor container

--- src/main/java/au/edu/cqu/se/hms/controllers/AdminPortalController.java
+++ src/main/java/au/edu/cqu/se/hms/controllers/AdminPortalController.java
@@ -12,6 +12,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.scene.image.Image;
@@ -24,13 +25,30 @@ import javafx.scene.layout.Pane;
  */
 public class AdminPortalController implements Initializable {
 
-    private MenuButton profileButton;
+    private final String selectedMenuStyle = "-fx-text-fill: #eee; -fx-font-size: 15px; -fx-background-color: #546e7a; -fx-border-color: transparent; -fx-underline: false;";
+    private final String unSelectedMenuStyle = "-fx-text-fill: #546e7a; -fx-font-size: 15px; -fx-border-color: transparent; -fx-underline: false;";
 
     private AuthenticationService authService;
     @FXML
     private Label nameLbl;
     @FXML
     private Pane sidebarPane;
+    @FXML
+    private Hyperlink doctorMenu;
+    @FXML
+    private Hyperlink assistantMenu;
+    @FXML
+    private Hyperlink adminMenu;
+    @FXML
+    private Pane doctorContainer;
+    @FXML
+    private Pane assistantContainer;
+    @FXML
+    private Pane adminContainer;
+    @FXML
+    private Pane doctorListContainer;
+    @FXML
+    private Pane saveDoctorCotaniner;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -47,10 +65,86 @@ public class AdminPortalController implements Initializable {
         }
         nameLbl.setText("Male".equals(user.getGender()) ? "Mr. " + user.getLastName() : "Mrs. " + user.getLastName());
         nameLbl.layoutXProperty().bind(sidebarPane.widthProperty().subtract(nameLbl.widthProperty()).divide(2));
+        showDoctorContainer();
     }
 
     @FXML
     private void handleLogout(ActionEvent event) {
         UIUtils.logout();
+    }
+
+    @FXML
+    private void handleDoctorMenu(ActionEvent event) {
+        showDoctorContainer();
+    }
+    
+    @FXML
+    private void handleAddDoctor(ActionEvent event) {
+        showAddDoctorContainer();
+    }
+    
+    @FXML
+    private void handleDoctorBackBtn(ActionEvent event) {
+        showDoctorListContainer();
+    }
+
+    private void showDoctorContainer() {
+        doctorMenu.setStyle(selectedMenuStyle);
+        assistantMenu.setStyle(unSelectedMenuStyle);
+        adminMenu.setStyle(unSelectedMenuStyle);
+
+        doctorContainer.setVisible(true);
+        assistantContainer.setVisible(false);
+        adminContainer.setVisible(false);
+        
+        showDoctorListContainer();
+    }
+    
+    private void showDoctorListContainer() {
+        saveDoctorCotaniner.setVisible(false);
+        doctorListContainer.setVisible(true);
+    }
+    
+    private void showAddDoctorContainer() {
+        saveDoctorCotaniner.setVisible(true);
+        doctorListContainer.setVisible(false);
+    }
+
+    @FXML
+    private void handleAssistantMenu(ActionEvent event) {
+        showAssistantContainer();
+    }
+
+    private void showAssistantContainer() {
+        doctorMenu.setStyle(unSelectedMenuStyle);
+        assistantMenu.setStyle(selectedMenuStyle);
+        adminMenu.setStyle(unSelectedMenuStyle);
+
+        doctorContainer.setVisible(false);
+        assistantContainer.setVisible(true);
+        adminContainer.setVisible(false);
+    }
+
+    @FXML
+    private void handleAdminMenu(ActionEvent event) {
+        showAdminContainer();
+    }
+
+    private void showAdminContainer() {
+        doctorMenu.setStyle(unSelectedMenuStyle);
+        assistantMenu.setStyle(unSelectedMenuStyle);
+        adminMenu.setStyle(selectedMenuStyle);
+
+        doctorContainer.setVisible(false);
+        assistantContainer.setVisible(false);
+        adminContainer.setVisible(true);
+    }
+
+    @FXML
+    private void handleAddAssistant(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleAddAdmin(ActionEvent event) {
     }
 }
